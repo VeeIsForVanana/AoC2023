82c6e804e57c37136cc641143f3ddd37de26f4b5
VeeIsForVanana <victorereyesgg@gmail.com>
2023-12-07 23:26:32 +0800

    change approach, instead of stripping non-numerics we map all numerics to a list and map all word-numerics to their numeric equivalents

--- day1/day1_2.py
+++ day1/day1_2.py
@@ -1,22 +1,21 @@
 import re
 
-WORD_TO_NUMERAL_DICT = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
-WORD_TO_NUMERAL_PATTERN = r'one|two|three|four|five|six|seven|eight|nine'
+WORD_TO_NUMERAL_DICT = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+WORD_TO_NUMERAL_PATTERN = r''
 NON_NUMERICALS_PATTERN = r'[A-z\s]'
+ALL_NUMERICALS_PATTERN = r'1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine'
 
-def replaceWordNumericals(line: str) -> str:
-    def wordToNumerical(matchobj: re.Match) -> str:
-        return str(WORD_TO_NUMERAL_DICT[matchobj.group(0)])
-    return re.sub(WORD_TO_NUMERAL_PATTERN, wordToNumerical, line)
-
-def stripNonNumericals(line: str) -> str:
-    return re.sub(NON_NUMERICALS_PATTERN, '', line)
+def listAllNumericals(line: str) -> list[str]:
+    def mapMatchToNumerical(match: str) -> str:
+        return WORD_TO_NUMERAL_DICT.get(match, match)
+    print(re.findall(ALL_NUMERICALS_PATTERN, line))
+    return list(map(mapMatchToNumerical, re.findall(ALL_NUMERICALS_PATTERN, line)))
 
 def getFirstAndLastCharacterAsInt(line: str) -> int:
-    return int(''.join([stripNonNumericals(replaceWordNumericals(line))[i] for i in [0, -1]]))
+    return int(''.join(listAllNumericals(line)[i] for i in [0, -1]))
 
 def main():
-    with open(".txt") as file:
+    with open("test_2.txt") as file:
         total: int = 0
         
         while((line := file.readline()) != ''):
