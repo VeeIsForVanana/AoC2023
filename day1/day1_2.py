0edb36696500e2c735f36d9fc5b520dedf1e6a5e
VeeIsForVanana <victorereyesgg@gmail.com>
2023-12-07 23:28:30 +0800

    day1 completed

--- day1/day1_2.py
+++ day1/day1_2.py
@@ -1,4 +1,4 @@
-import re
+import regex
 
 WORD_TO_NUMERAL_DICT = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
 WORD_TO_NUMERAL_PATTERN = r''
@@ -8,14 +8,13 @@ ALL_NUMERICALS_PATTERN = r'1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|e
 def listAllNumericals(line: str) -> list[str]:
     def mapMatchToNumerical(match: str) -> str:
         return WORD_TO_NUMERAL_DICT.get(match, match)
-    print(re.findall(ALL_NUMERICALS_PATTERN, line))
-    return list(map(mapMatchToNumerical, re.findall(ALL_NUMERICALS_PATTERN, line)))
+    return list(map(mapMatchToNumerical, regex.findall(ALL_NUMERICALS_PATTERN, line, overlapped=True)))
 
 def getFirstAndLastCharacterAsInt(line: str) -> int:
     return int(''.join(listAllNumericals(line)[i] for i in [0, -1]))
 
 def main():
-    with open("test_2.txt") as file:
+    with open("input.txt") as file:
         total: int = 0
         
         while((line := file.readline()) != ''):
