00463a282dc91181f5739edb4310ffda6c468d46
Victor Edwin Reyes <95967340+VeeIsForVanana@users.noreply.github.com>
2023-12-14 12:48:53 +0000

    solve day2 pat 2

---
+++ day2/day2_2.py
@@ -0,0 +1,86 @@
+from sys import argv
+from enum import StrEnum, auto
+from functools import reduce
+from math import prod
+
+class Color(StrEnum):
+    RED = auto()
+    GREEN = auto()
+    BLUE = auto()
+
+CONST_MAX_RED = 12
+CONST_MAX_GREEN = 13
+CONST_MAX_BLUE = 14
+
+class Line:
+    
+    string: str
+    index: int
+    rounds: list[dict[Color: int]]
+    maxPerColor: dict[Color: int]
+
+    def __init__(self, inString) -> None:
+        self.string = inString
+        self.index = 0
+        self.rounds = []
+
+    def __str__(self) -> str:
+        return f"Round {self.index} with {self.rounds}"
+
+    def parseLine(self):
+        gameString, allRoundsString = self.string.split(":")
+        self.index = int(gameString[5:])
+        
+        def fromRoundToDictRound(round: str) -> dict[Color: int]:
+
+            def fromColorCountToTuple(colorCount: str) -> (Color, int):
+                colorCount = colorCount.strip()
+                match colorCount.split()[1]:
+                    case Color.RED:
+                        return (Color.RED, int(colorCount.split(" ")[0]))
+                    case Color.BLUE:
+                        return (Color.BLUE, int(colorCount.split(" ")[0]))
+                    case Color.GREEN:
+                        return (Color.GREEN, int(colorCount.split(" ")[0]))
+
+            return dict(map(fromColorCountToTuple, round.split(',')))
+
+        rounds = allRoundsString.split(";")
+        self.rounds = list(map(fromRoundToDictRound, rounds))
+        return self
+
+    def findMaxValuesPerColor(self) -> list[int]:
+        valuesDict: dict[Color: list[int]] = {Color.RED: [], Color.GREEN: [], Color.BLUE: []}
+        
+        # takes an dict[Color: int] and adds the value for each color to the corresponding list in the dict
+        def buildColorList(acc: dict[Color: list[int]] , elem: dict[Color: int]):
+            for key in elem.keys():
+                acc[key].append(elem[key])
+            return acc
+        
+        valuesDict = reduce(buildColorList, self.rounds, valuesDict)
+        return list(map(lambda lst: max(lst), valuesDict.values()))
+
+    def returnValidSum(self) -> int:
+        
+        def validityCheck(inDict: dict) -> bool:
+            print(f'inDict: {inDict}')
+            print(f'sum: {sum(inDict.values()) <= CONST_MAX_BLUE + CONST_MAX_GREEN + CONST_MAX_RED}')
+            print(f'red: {inDict.get(Color.RED, 0) <= CONST_MAX_RED}')
+            print(f'green: {inDict.get(Color.GREEN, 0) <= CONST_MAX_GREEN}')
+            print(f'blue: {inDict.get(Color.BLUE, 0) <= CONST_MAX_BLUE}')
+            return (sum(inDict.values()) <= CONST_MAX_BLUE + CONST_MAX_GREEN + CONST_MAX_RED) & (inDict.get(Color.RED, 0) <= CONST_MAX_RED) & (inDict.get(Color.GREEN, 0) <= CONST_MAX_GREEN) & (inDict.get(Color.BLUE, 0) <= CONST_MAX_BLUE)
+        
+        print(self.rounds)
+        return self.index if all(list(map(validityCheck, self.rounds))) else 0
+
+def main(filename: str) -> int:
+    with open(filename) as file:
+        total = 0
+        while((line := file.readline()) != ""):
+            print(line) 
+            print(total := total + prod(Line(line).parseLine().findMaxValuesPerColor()))
+        return total
+
+if __name__ == "__main__":
+    main(argv[1])
